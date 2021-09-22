b1c7ee929862a11c175bad5fe27866e13e64c48b
Marko Punnar <marko@aretaja.org>
2021-09-22 14:30:42 +0300

    Rename interfaces

--- dev_interfaces.go
+++ dev_interfaces.go
@@ -1,30 +1,30 @@
 package godevman
 
 // Get system info
-type DevSystem interface {
+type DevSys interface {
 	System(targets []string) (system, error)
 }
 
-// Interfaces
-
-// Get interface info
-type DevIfInfo interface {
+// Functionality related to interfaces
+type DevIfs interface {
 	IfInfo(targets []string, idx ...string) (map[string]*ifInfo, error)
 	// Get ifNumber
 	IfNumber() (int64, error)
 	// Get interfaces stack info
 	IfStack() (ifStack, error)
+	// Set interface admin status
+	SetIfAdmStat(idx []string, status string) error
 }
 
-// Get inventory info
-type DevInvInfo interface {
+// Functionality related to inventory
+type DevInv interface {
 	InvInfo(targets []string, idx ...string) (map[string]*invInfo, error)
 	// Get interface to inventory relations
 	IfInventory() (map[int]int, error)
 }
 
-// Get dot1q vlan info
-type DevVlanInfo interface {
+// Functionality related to dot1q vlans
+type DevVlan interface {
 	D1qVlans() (map[string]string, error)
 	// Get dot1q vlans
 	BrPort2IfIdx() (map[string]int, error)
@@ -32,19 +32,19 @@ type DevVlanInfo interface {
 	D1qVlanInfo() (map[string]*d1qVlanInfo, error)
 }
 
-// Get IP info
-type DevIpInfo interface {
+// Functionality related to IP addresses
+type DevIp interface {
 	IpInfo(targets []string, ip ...string) (map[string]*ipInfo, error)
 	IpIfInfo(ip ...string) (map[string]*ipIfInfo, error)
 }
 
-// Get IPv6 info
-type DevIp6Info interface {
+// Functionality related to Get IPv6 addresses
+type DevIp6 interface {
 	Ip6IfDescr(idx ...string) (map[string]string, error)
 }
 
 // Get OSPF info
-type DevOspfInfo interface {
+type DevOspf interface {
 	OspfAreaRouters() (map[string][]string, error)
 	OspfAreaStatus() (map[string]string, error)
 	OspfNbrStatus() (map[string]string, error)
