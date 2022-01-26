6c296e9699d52bdd523e70afa68e9e3e141ad850
Marko Punnar <marko@aretaja.org>
2022-01-26 14:56:02 +0200

    Able to use CLI on Juniper devices

--- dev_juniper.go
+++ dev_juniper.go
@@ -11,3 +11,55 @@ func (sd *deviceJuniper) SwVersion() (string, error) {
 	r, err := sd.getone(oid)
 	return r[oid].OctetString, err
 }
+
+// Prepare CLI session parameters
+func (sd *deviceJuniper) cliPrepare() (*CliParams, error) {
+	defParams, err := sd.snmpCommon.cliPrepare()
+	if err != nil {
+		return nil, err
+	}
+
+	params := defParams
+
+	// make device specific changes to default parameters
+	if params.PromptRe == "" {
+		params.PromptRe = `(>|#|\%) ?$`
+	}
+	if params.ErrRe == "" {
+		params.ErrRe = `(?im)(error|unknown|invalid|failed|timed out)`
+	}
+	if params.PreCmds == nil {
+		params.PreCmds = []string{
+			"",
+			"set cli complete-on-space off",
+			"set cli screen-length 0",
+		}
+	}
+
+	return params, nil
+}
+
+// Execute cli commands
+func (sd *deviceJuniper) RunCmds(c []string) ([]string, error) {
+	p, err := sd.cliPrepare()
+	if err != nil {
+		return nil, err
+	}
+
+	err = sd.startCli(p)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := sd.cliCmds(c)
+	if err != nil {
+		return out, err
+	}
+
+	err = sd.closeCli()
+	if err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
